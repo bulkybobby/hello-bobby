bd792af2b09cd33536deaed3d6c5b4e9de577e5f
bulkybobby <bulkybobby92@gmail.com>
2022-08-05 23:40:58 +0900

    page test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import test from './test.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
+      <nav className="App-nav">
+        <ul>
+          <li>about</li>
+          <li>home</li>
+          <li>contact</li>
+        </ul>
+      </nav>
       <header className="App-header">
-        <img src={test} className="App-logo" alt="logo" />
+        <h1>This is header</h1>
       </header>
+      <body className="App-body">
+        <h1>This is body</h1>
+      </body>
+      <footer>
+        <h1>This is footer</h1>
+      </footer>
     </div>
   );
 }
